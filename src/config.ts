4622805de01f6eac0e0c510717a65e81cf1cd005
Andy <byronandres3112@gmail.com>
2026-09-13 11:42:23 -0700

    merge: resolver conflictos

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,4 @@ export const LOGO_URL: string = '{{LOGO_URL}}'
 export const BANNER_URL: string = '{{BANNER_URL}}'
 export const PUNTOS_META = {{PUNTOS_META}}
 export const WHATSAPP: string = '{{WHATSAPP}}'
-<<<<<<< HEAD
 export const INSTAGRAM: string = '{{INSTAGRAM}}'
-=======
-export const INSTAGRAM: string = '{{INSTAGRAM}}'
->>>>>>> af7dba40b3503a13b64094dc48d85eb67f7a44ec
